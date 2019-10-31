3ce3b8825260d5e3cb180e7b59767c2a96a4e741
red-gezi <506065448@qq.com>
2019-10-31 09:50:31 +0800

    10.31增量更新

--- 东方格致录/Assets/Script/9_GloableScene/Test/test.cs
+++ 东方格致录/Assets/Script/9_GloableScene/Test/test.cs
@@ -11,6 +11,13 @@ public class test : MonoBehaviour
 {
     [ShowInInspector]
     public CardSet cardSet => AgainstInfo.cardSet;
+    public float a, b, c;
+    [ShowInInspector] public float exp_a => Mathf.Exp(a);
+    [ShowInInspector] public float exp_b => Mathf.Exp(b);
+    [ShowInInspector] public float exp_c => Mathf.Exp(c);
+    [ShowInInspector] public float P_A => exp_a / (exp_a + exp_b + exp_c);
+    [ShowInInspector] public float P_B => exp_b / (exp_a + exp_b + exp_c);
+    [ShowInInspector] public float P_C => exp_c / (exp_a + exp_b + exp_c);
     [Button]
     public void test1()
     {
@@ -18,7 +25,7 @@ public class test : MonoBehaviour
         var a = AgainstInfo.cardSet[Orientation.Down];
         var b = a[RegionTypes.Leader];
         b.Add(new Card());
-       
+
         print("yaya");
     }
 }
--- 东方格致录/Assets/Script/9_GloableScene/UI/NoticeControl.cs
+++ 东方格致录/Assets/Script/9_GloableScene/UI/NoticeControl.cs
@@ -8,14 +8,16 @@ namespace Control
     {
         public class NoticeControl : MonoBehaviour
         {
-            Animator Anim;
+            //Animator Anim;
             public float Aplha = 0;
-            public GameObject[] NoticeTex;
-            //public GameObject NoticeModel;
-            void Start() => Anim = GetComponent<Animator>();
-            void Update() => NoticeTex.ForEach(x => x.GetComponent<Image>().material.SetFloat("_Value", Aplha));
-            [Button]
-            public void AnimPlay() => Anim.SetTrigger("Play");
+            //public GameObject[] NoticeTex;
+            ////public GameObject NoticeModel;
+            //void Start() => Anim = GetComponent<Animator>();
+            //void Update() => NoticeTex.ForEach(x => x.GetComponent<Image>().material.SetFloat("_Value", Aplha));
+            //[Button]
+            //public void AnimPlay() => Anim.SetTrigger("Play");
+            void Update() => GetComponent<Image>().material.SetFloat("_Value", Aplha);
+
         }
     }
 }
